9f175b840e7687f36307cae671e9b73c5a0e695d
hanjsen <joakim.hansen@savar.se>
2020-12-03 11:59:20 +0100

    Day 3.2

--- src/com/joakimhansen/DayThree.java
+++ src/com/joakimhansen/DayThree.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,9 +12,12 @@ public class DayThree {
 
     private final File input = new File("/Users/jockehansen/workspace/advent-of-code-2020/input-daythree.txt");
     private final List<String> slope = new ArrayList<>();
+    private final List<Integer> horizontalAttackAngle = Arrays.asList(1, 3, 5, 7, 1);
+    private final List<Integer> verticalAttackAngle = Arrays.asList(1, 1, 1, 1, 2);
+    private final List<Integer> treesHitPerAttempt = new ArrayList<>();
+    private int tobogganPosition;
     private int slopeWidth;
     private int treesHit;
-    private int tobogganHorizontalAlignment;
 
     public DayThree() {
     }
@@ -22,41 +26,64 @@ public class DayThree {
         try {
             parseFile();
             setSlopeWidth();
-            rideTobogganToAirport();
-            System.out.println(treesHit);
+            startAttempts();
+            printAggregatedTreesHit();
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
+    }
+
+    private void startAttempts() {
+        for (int attempt = 0; attempt < horizontalAttackAngle.size(); attempt++) {
+            resetRideStats();
+            rideTobogganToAirport(attempt);
+            System.out.println("Hit " + treesHit + " trees, current attack angle {" + horizontalAttackAngle.get(attempt) + " - " + verticalAttackAngle.get(attempt) +"}");
+            treesHitPerAttempt.add(treesHit);
+        }
+    }
+
+    private void resetRideStats() {
+        tobogganPosition = 0;
+        treesHit = 0;
+    }
+
+    private void printAggregatedTreesHit() {
+        long aggregatedCrashes = 1;
+        for (Integer trees : treesHitPerAttempt) {
+            aggregatedCrashes *= trees;
+        }
+        System.out.println(aggregatedCrashes);
     }
 
     private void setSlopeWidth() {
         slopeWidth = slope.get(0).length();
     }
 
-    private void rideTobogganToAirport() {
-        for (String slopeLevel : slope) {
+    private void rideTobogganToAirport(Integer attempt) {
+        int attackAngle = verticalAttackAngle.get(attempt);
+        for (int downhillLevel = 0; downhillLevel < slope.size(); downhillLevel += attackAngle) {
             checkOffpist();
-            checkIfTreeHit(slopeLevel);
-            tobogganHorizontalAlignment += 3;
+            checkIfTreeHit(slope.get(downhillLevel));
+            tobogganPosition += horizontalAttackAngle.get(attempt);
         }
     }
 
     private void checkIfTreeHit(String slopeLevel) {
-        char currentPosition = slopeLevel.charAt(tobogganHorizontalAlignment);
-        
+        char currentPosition = slopeLevel.charAt(tobogganPosition);
+
         if (isTree(currentPosition)) {
             treesHit++;
         }
     }
 
     private void checkOffpist() {
-        if (tobogganHorizontalAlignment >= slopeWidth) {
+        if (tobogganPosition >= slopeWidth) {
             repeatSlope(slopeWidth);
         }
     }
 
     private void repeatSlope(int slopeWidth) {
-        tobogganHorizontalAlignment = (tobogganHorizontalAlignment - slopeWidth);
+        tobogganPosition = (tobogganPosition - slopeWidth);
     }
 
     private boolean isTree(char charAt) {
